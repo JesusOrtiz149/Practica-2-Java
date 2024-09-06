d1cd156eec89efcd3839f659ebc040a39c63e08a
Jesús Ortiz <alu.22130574@correo.itlalaguna.edu.mx>
2024-09-06 12:55:53 -0600

    Todas las operaciones funcionan correcamente y también los botones

--- src/Principal.java
+++ src/Principal.java
@@ -17,13 +17,76 @@ public class Principal {
     private JTextField txtRM;
     private JPanel pnlOps;
     private JTextField txtRD;
-
+    private JButton btnBorrar;
+    int n1,  n2;
 
     public Principal() {
         btnCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-    // a
+
+                if(!txtNumS2.getText().equals("") && !txtNumS1.getText().equals(""))  {
+                     n1 = Integer.parseInt(txtNumS1.getText());
+                     n2 = Integer.parseInt(txtNumS2.getText());
+                    txtRS.setText(Integer.toString(n1 + n2));
+                    n1 = 0;
+                    n2 = 0;
+                    //txtRS.setText("0");
+                }
+                else{
+                }
+
+                if(!txtNumR2.getText().equals("") && !txtNumR1.getText().equals("")) {
+                    n1 = Integer.parseInt(txtNumR1.getText());
+                    n2 = Integer.parseInt(txtNumR2.getText());
+                    txtRR.setText(Integer.toString(n1 - n2));
+                    n1 = 0;
+                    n2 = 0;
+                }
+                else{
+
+                }
+                if(!txtNumM2.getText().equals("") && !txtNumM1.getText().equals("")) {
+                    n1 = Integer.parseInt(txtNumM1.getText());
+                    n2 = Integer.parseInt(txtNumM2.getText());
+                    txtRM.setText(Integer.toString(n1 * n2));
+                    n1 = 0;
+                    n2 = 0;
+                }
+                else {
+
+                }
+                if(!txtNumD2.getText().equals("") && !txtNumD1.getText().equals("")) {
+                    double n3;
+                    double n4;
+                    n3 = Double.parseDouble(txtNumD1.getText());
+                    n4 = Double.parseDouble(txtNumD2.getText());
+
+                    txtRD.setText(Double.toString(n3 / n4));
+                    n3 = 0;
+                    n4 = 0;
+                }
+                else {
+
+                }
+            }
+        });
+
+        btnBorrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtNumS1.setText("");
+                txtNumS2.setText("");
+                txtNumR1.setText("");
+                txtNumR2.setText("");
+                txtNumM1.setText("");
+                txtNumM2.setText("");
+                txtNumD1.setText("");
+                txtNumD2.setText("");
+                txtRS.setText("");
+                txtRR.setText("");
+                txtRM.setText("");
+                txtRD.setText("");
             }
         });
     }
